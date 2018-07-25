edac0933f7b3b1a1195211f58ec287bc1231d5ca
Allan Guwatudde <allanguwatudde@Allans-MacBook-Pro.local>
2018-07-25 11:02:49 +0300

    [Chore #159294934] Add project settings for multiple environments

--- authors/settings/base.py
+++ authors/settings/base.py
@@ -77,29 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'authors.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-db_url = os.getenv("DATABASE_URL")
-parsed_url = urlparse(db_url)
-dbname = parsed_url.path[1:]
-username = parsed_url.username
-hostname = parsed_url.hostname
-pwd = parsed_url.password
-port_number = parsed_url.port
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': dbname,
-        'USER': username,
-        'PASSWORD': pwd,
-        'HOST': hostname,
-        'PORT': port_number,
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
