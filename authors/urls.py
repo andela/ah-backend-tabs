973ea5b19e507e60def1550e9ec89eaec58e6eff
Allan Guwatudde <allanguwatudde@Allans-MacBook-Pro.local>
2018-07-27 17:08:28 +0300

    [Feature ##159082306] Add descriptive validation errors when a user tries to signup

--- authors/urls.py
+++ authors/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/', include('authors.apps.authentication.urls', namespace='authentication')),
+    path('api/', include('authors.apps.authentication.urls')),
+
 ]
